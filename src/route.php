cb8bddbb9dbd0cc067afea24e3eba19a92e60c52
junfei <junfei@qq.com>
2020-07-13 17:20:22 +0800

    i

--- src/route.php
+++ src/route.php
@@ -1,9 +1,13 @@
 <?php
 use think\facade\Route;
 Route::group('', function () {
-
     Route::rule('/news/index', '\mnews\controller\News@index' );
     Route::rule('/news/show', '\mnews\controller\News@show');
+    Route::rule('/news/save', '\mnews\controller\News@save' );
 
 });
-    //->middleware([\app\middleware\Auth::class],'home');
+//
+//Route::group('', function () {
+//    Route::rule('/news/save', '\mnews\controller\News@save' );
+//
+//})->middleware('auth','u');
